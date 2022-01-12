ce5f39f158d7218d5e2654fd2cb341b43c23562b
Prince Arthur <geniusapc@gmail.com>
2022-01-12 13:24:31 +0100

    feat: add root route response and env.example

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const errorHandler = require('../middlewares/errorHandler');
 
 module.exports = (app) => {
   app.use('/api/v1/', v1);
+  app.use('/', (req, res)=>{
+    return res.send("ok")
+  } );
   app.use(NotFound);
   app.use(errorHandler);
 };
